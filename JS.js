bcb50476d96c867ab8c46b9c334bc6a4a54e461a
Dharani04 <35607122+Dharani04@users.noreply.github.com>
2019-11-01 12:32:36 +0530

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,32 @@
+﻿function add()
+{
+    var list = document.createElement("li");
+    var x = document.getElementById("action").value;
+    list.innerHTML = x;
+    var c = document.createElement("input");
+    var b = document.createElement("button");
+    b.setAttribute("class", "x");
+    list.appendChild(b);
+    b.innerText = "X";
+    b.addEventListener("click", del);
+    c.type = "checkbox";
+    c.setAttribute("class", "check");
+    c.style.visibility = "hidden";
+    list.appendChild(c);
+  
+    if (x != "")
+        document.getElementById("empty").appendChild(list).addEventListener("click", al);
+    else
+        alert("Enter something!")
+    document.getElementById("action").value = "";
+    function al()
+    {
+        c.style.visibility = "visible";
+        c.checked = "true";
+        list.setAttribute("class", "checked");
+    }
+    function del()
+    {
+        list.remove(globalThis);
+    }
+}
